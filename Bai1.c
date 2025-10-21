336a59b0ad733c7cd6da6b4e8171bc2e5c2d87f1
Trần Kim Tây <trankimtayyy@gmail.com>
2025-10-21 09:33:44 +0700

    hehehe

--- Bai1.c
+++ Bai1.c
@@ -1,32 +1,21 @@
 #include <stdio.h>
 
-int main() {
-    int month, year;
+void inSoNgayTrongThang() {
+    int thang;
+    printf("Nhap thang: ");
+    scanf("%d", &thang);
 
-    printf("Nhap vao thang (1-12): ");
-    scanf("%d", &month);
-
-    switch (month) {
+    switch (thang) {
         case 1: case 3: case 5: case 7: case 8: case 10: case 12:
-            printf("Thang %d co 31 ngay.\n", month);
+            printf("Thang %d co 31 ngay\n", thang);
             break;
-
         case 4: case 6: case 9: case 11:
-            printf("Thang %d co 30 ngay.\n", month);
+            printf("Thang %d co 30 ngay\n", thang);
             break;
-
         case 2:
-            printf("Nhap vao nam: ");
-            scanf("%d", &year);
-            if ((year % 400 == 0) || (year % 4 == 0 && year % 100 != 0))
-                printf("Thang 2 nam %d co 29 ngay (nam nhuan).\n", year);
-            else
-                printf("Thang 2 nam %d co 28 ngay.\n", year);
+            printf("Thang 2 co 28 ngay (khong tinh nam nhuan)\n");
             break;
-
         default:
-            printf("Thang khong hop le! Vui long nhap tu 1 den 12.\n");
+            printf("Thang khong hop le!\n");
     }
-
-    return 0;
 }
